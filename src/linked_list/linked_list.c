4e90b36572b55df7d981ac570e76eabdcf96dae9
Timsley <491528529@qq.com>
2016-04-14 19:48:12 +0800

    Timsley 2016-04-14 : solution(142. Linked List Cycle II)

--- src/linked_list/linked_list.c
+++ src/linked_list/linked_list.c
@@ -593,6 +593,51 @@ bool hasCycle(struct ListNode *head)
     return false;
 }
 
+/***************************************************************************************
+*****                             142. Linked List Cycle II
+*****       Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+*****       Note: Do not modify the linked list.
+
+*****       Follow up:
+*****       Can you solve it without using extra space?
+****************************************************************************************/
+struct ListNode *detectCycle(struct ListNode *head) 
+{
+    struct ListNode *fast, *slow;
+    bool fgHasCycle = false;
+
+    if(!head)
+        return NULL;
+
+    fast = head;
+    slow = head;
+    
+    while(fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+
+        if(fast == slow)
+        {
+            fgHasCycle = true;
+            break;
+        }
+    }
+
+    if(true == fgHasCycle)
+    {
+        fast = head;
+        while(fast)
+        {
+            if(fast == slow)
+                return fast;
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+    else
+        return NULL;
+}
 
 
 void link_list_main_test(void)
